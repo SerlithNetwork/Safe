3ca7b40a22bf82c16fb7ba35a7d0e7862b0ecdc7
Biquaternions <biquaternions@serlith.net>
2025-06-15 12:17:07 -0500

    Add command descriptions

--- src/main/java/net/milkbowl/vault/SafeBootstrap.java
+++ src/main/java/net/milkbowl/vault/SafeBootstrap.java
@@ -12,8 +12,8 @@ public class SafeBootstrap implements PluginBootstrap {
     @Override
     public void bootstrap(@NotNull BootstrapContext context) {
         context.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
-            commands.registrar().register(SafeCommandLoader.buildInfoCommand());
-            commands.registrar().register(SafeCommandLoader.buildConvertCommand());
+            commands.registrar().register(SafeCommandLoader.buildInfoCommand(), "Displays information about Safe");
+            commands.registrar().register(SafeCommandLoader.buildConvertCommand(), "Converts all economy data in <source> and dumps it into <target>");
         });
     }
 
